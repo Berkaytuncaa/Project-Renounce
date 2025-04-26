5986b6a27ba547af3e71f3286298f04329e1e87f
Berkay Tunca <berkaytunca40@gmail.com>
2025-04-26 22:57:44 +0300

    organize files

---
+++ Assets/Scripts/PopUp/PanelCloseHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelClose : MonoBehaviour
+{
+    private void Start()
+    {
+        // Get the Button component and add a listener to its onClick event
+        Button closeButton = GetComponent<Button>();
+        if (closeButton != null)
+        {
+            closeButton.onClick.AddListener(ClosePanel);
+        }
+    }
+
+    private void ClosePanel()
+    {
+        // Destroy the parent panel GameObject
+        Destroy(transform.parent.gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        // Clean up the listener when the object is destroyed
+        Button closeButton = GetComponent<Button>();
+        if (closeButton != null)
+        {
+            closeButton.onClick.RemoveListener(ClosePanel);
+        }
+    }
+}
